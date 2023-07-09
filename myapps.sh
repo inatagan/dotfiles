3af8aa31909ade183a2ae0019b35baf2cddaa38e
nagatani <22266171+inatagan@users.noreply.github.com>
2023-07-09 16:28:55 -0300

    Update myapps.sh

--- myapps.sh
+++ myapps.sh
@@ -4,8 +4,8 @@
 #update sources and uprade default base system
 sudo apt update && sudo apt upgrade
 #install apps
-sudo apt install sakura firefox-esr redshift redshift-gtk conky conky-all htop tmux curl qbittorrent vlc
+sudo apt install xfce4-whiskermenu-plugin sakura firefox-esr redshift redshift-gtk conky conky-all htop tmux curl qbittorrent vlc lm-sensors grub-customizer
 #install starship shell prompt
-sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+#sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 #Adittional packages
-#grub-customizer lightdm-gtk-greeter lightdm-gtk-greeter-settings lm-sensors
+#lightdm-gtk-greeter lightdm-gtk-greeter-settings
