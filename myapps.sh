200138d61385ac4b8956c2e0f3e12a384a1c61e6
ina <22266171+inatagan@users.noreply.github.com>
2023-07-11 19:37:39 -0300

    updated default packages to install

--- myapps.sh
+++ myapps.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # change permission
 # chmod +x myapps.sh
-#update sources and uprade default base system
+###update sources and uprade default base system
 sudo apt update && sudo apt upgrade
-#install apps
+### install my default apps
 sudo apt install xfce4-whiskermenu-plugin sakura firefox-esr redshift redshift-gtk conky conky-all htop tmux curl qbittorrent vlc lm-sensors grub-customizer lightdm-gtk-greeter lightdm-gtk-greeter-settings
-#install starship shell prompt
-#sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-#Adittional packages
+### install bluetooth packages
+sudo apt install bluez-* pulseaudio-module-bluetooth blueman
+sudo adduser $USER bluetooth
+### it's recomended to reboot the system now
